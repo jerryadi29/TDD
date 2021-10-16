5e236ef162079a993539fbdc4740fa2e83c33980
aditya rout <aditrout29@gmail.com>
2021-10-16 16:19:42 +0530

    all test cases passed

--- TDD/firstC.cpp
+++ TDD/firstC.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+    
 
 int Add(string num){
 
-
     if(num.length()==0){
-            cout << "user string is empty"<<"\n";
+            cout << "user string is empty";
             return 0;
         }
 
-   int strToNum=stoi(num);
+    int sum=0;
+    int temp;
+    for(int i=1;i<num.length()-1;++i){
+      
+        temp=num[i]-'0';
 
-   try{
-      if(strToNum<0){
-        throw strToNum;
-      }
-   }
-   catch(int strToNum){
-    cout<<"negative not allowed"<<"\n";
-   }	
-	 return strToNum;
+        if(temp<0){
+            cout<<"negative not allowed"<<"\n";
+            return 0;
+        }
+    
+        if((temp>=0 && temp <10)){
+            sum=sum+temp;
+        }
+    }
+
+
+    return sum;  
+
+
 }
 
 int main()
